4ddeb3a582892a9597f7765d73531b5938255096
benbrooks15 <102412238+EquaFerrous@users.noreply.github.com>
2022-08-04 18:03:32 +0100

    Updated RemoveTree command
    - Added coordinate functionality to RemoveTree command.

--- src/me/equaferrous/woodchoppingtrials/commands/RemoveTreeCommand.java
+++ src/me/equaferrous/woodchoppingtrials/commands/RemoveTreeCommand.java
@@ -2,6 +2,8 @@ package me.equaferrous.woodchoppingtrials.commands;
 
 import me.equaferrous.woodchoppingtrials.trees.TreeManager;
 import me.equaferrous.woodchoppingtrials.utility.MessageSystem;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,15 +16,52 @@ public class RemoveTreeCommand implements CommandExecutor {
             MessageSystem.negativeMessage(commandSender, "Command only usable by players.");
             return true;
         }
+        if (args.length != 0 && args.length != 3) {
+            throwInvalidCommand(commandSender);
+            return true;
+        }
 
         Player player = (Player) commandSender;
-        if (TreeManager.getInstance().deleteTree(player.getLocation())) {
-            MessageSystem.positiveMessage(player, "Tree removed.");
+
+        Location location;
+        if (args.length == 0) {
+            location = player.getLocation();
         }
         else {
-            MessageSystem.negativeMessage(player, "No tree found.");
+            double x;
+            double y;
+            double z;
+            try {
+                x = Double.parseDouble(args[0]);
+                y = Double.parseDouble(args[1]);
+                z = Double.parseDouble(args[2]);
+            }
+            catch (NumberFormatException e) {
+                throwInvalidCommand(commandSender);
+                return true;
+            }
+
+            location = new Location(player.getWorld(), x, y, z);
         }
 
+        executeCommand(location, commandSender);
+
         return true;
+    }
+
+    // ----------------------------------------
+
+    private void throwInvalidCommand(CommandSender sender) {
+        MessageSystem.negativeMessage(sender, "Correct usage: "+ ChatColor.LIGHT_PURPLE +"/removeTree [<x> <y> <z>]");
+    }
+
+    private void executeCommand(Location location, CommandSender sender) {
+        if (TreeManager.getInstance().deleteTree(location)) {
+            String locationText = location.getBlockX() +" "+ location.getBlockY() +" "+ location.getBlockZ();
+            MessageSystem.positiveMessage(sender, "Tree removed at "+ locationText +".");
+        }
+        else {
+            MessageSystem.negativeMessage(sender, "No tree found at this location.");
+        }
     }
 }
